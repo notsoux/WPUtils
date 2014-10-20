6788162107c4e0681fc4d1b908daf427ee15156a
notsoux <wellstaff@gmail.com>
2014-10-20 14:46:42 +0200

    Initial Commit

---
+++ WPUtils/WPUtils.h
@@ -0,0 +1,19 @@
+//
+//  WPUtils.h
+//  WPUtils
+//
+//  Created by William Pompei on 20/10/14.
+//  Copyright (c) 2014 William Pompei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WPUtils.
+FOUNDATION_EXPORT double WPUtilsVersionNumber;
+
+//! Project version string for WPUtils.
+FOUNDATION_EXPORT const unsigned char WPUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WPUtils/PublicHeader.h>
+
+
